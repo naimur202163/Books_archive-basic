43c799d20e0edc45d138e350ea15fab3c929647b
Naimur Rahman <you@example.com>
2021-09-02 03:59:00 +0600

    done

--- script.js
+++ script.js
@@ -15,32 +15,50 @@ const InputsItems=()=>{
 
 // Displying Books in the Windows
 const displayBooksList=(books)=>{
-    const api=books.docs
+    const api=books.docs;
     const displyBooks=document.getElementById("disply-book");
+    displyBooks.textContent='';
 
     api.forEach(book=>{
             console.log(book)
-        //    creating element
+        //    creating new element
             const div=document.createElement('div');
             div.classList.add('col');
             div.innerHTML=`  <div class="card">
             <img  src=" https://covers.openlibrary.org/b/id/${book.cover_i}-L.jpg" width="150" height="300px" class="card-img-top" alt="...">
             <div class="card-body">
-              <h5 class="card-title">${book.title}</h5>
-              <h6>Publisher ${book.publisher} ${book.publish_year}</h6>
-              <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content.</p>
+              <h2 class="card-title">${book.title}</h2>
+               <h3>${book.author_name}</h3>
+              <h4>Publisher ${book.publisher} </h4>
+                <p>${book.contributor}</p>
+              <p >${book.publish_date}</p>
+              <h5> Language ${book.language}</h5>
             </div>
           </div>`;
         //   Append Child from Div
           displyBooks.appendChild(div);
     });
     // Displying Search Result
-    const displySearchResult=document.getElementById("search-results");
-    const div=document.createElement("div");
-    div.classList.add("h2")
-    div.innerHTML=`Ressult Found <h3>${books.numFound}</h3> `;
-    displySearchResult.appendChild(div)
+    const showResult=books.numFound
+        if(showResult===0){
+          const displySearchResult=document.getElementById("search-results");
+          // clear Data
+              displySearchResult.textContent="";
+          const div=document.createElement("div");
+          div.classList.add("h2")
+          div.innerHTML=`Ressult Not Found <h3>${showResult}</h3> `;
+          displySearchResult.appendChild(div)
+        }else{
+          const displySearchResult=document.getElementById("search-results");
+          // clear Data
+          displySearchResult.textContent="";
+          const div=document.createElement("div");
+          div.classList.add("h2")
+          div.innerHTML=`Ressult Found <h3>${showResult}</h3> `;
+          displySearchResult.appendChild(div)
+        };
+   
 
-}
+};
 
 // cover_i
